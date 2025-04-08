3beb481a6dc6de5fe3b444f0fa39ae9b6d063592
Lukys Bartos <Wolfy.141@seznam.cz>
2025-04-08 16:18:12 +0200

    Initial commit

---
+++ src/main/java/com/aplikace/Pujcovna_Zimniho_Vybaveni/PujcovnaZimnihoVybaveniApplication.java
@@ -0,0 +1,13 @@
+package com.aplikace.Pujcovna_Zimniho_Vybaveni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PujcovnaZimnihoVybaveniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PujcovnaZimnihoVybaveniApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aplikace/Pujcovna_Zimniho_Vybaveni/PujcovnaZimnihoVybaveniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aplikace.Pujcovna_Zimniho_Vybaveni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PujcovnaZimnihoVybaveniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
