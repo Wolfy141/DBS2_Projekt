6db61ee9377d20dd3aebc7fd6429224f4ca6e6ce
Spy <your.email@example.com>
2025-04-17 01:52:48 +0200

    Clean

--- src/main/resources/static/scripts/regLogScript.js
+++
@@ -1,3 +0,0 @@
-/*vykreslení buď toho nebo onoho*/
-/*vykreslí se přihlášení, pokud se klikne na registraci, vykreslí se registrace a smaže se přihlášení*/
-/*po registraci je nutný se přihlásit, po přihlášení redirect na hlavní stránku/na profil*/
--- src/main/resources/static/scripts/vybScript.js
+++
@@ -1,2 +0,0 @@
-/*potřebuju script kterej zobrazí vybavení který je jen určitý kategorie*/
-/*přidání elementů do správnýho místa, za každej jeden záznam vybavení, asi v foreach cyklu*/
